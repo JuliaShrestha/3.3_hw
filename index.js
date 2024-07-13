5d018f28a24e2dbf6013e326a0b0011a3ccac1fd
JuliaShrestha <juliashrestha215@gmail.com>
2024-07-13 17:06:03 +0300

    Numbers

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict"
+const number = 10369;
+
+let numStr = number.toString();
+console.log(numStr);
+
+let numSpl = numStr.split('');
+console.log(numSpl);
+
+let result = numSpl.join(' ');
+console.log(result);
